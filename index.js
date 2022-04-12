3e0dfd0b2368bd3100705f73f72fbe684e5b0189
Rai <rai.maxi@hotmail.com>
2022-04-12 14:06:54 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-function textReturn (text){
-    console.log(text)
+function somarNotas (nota1, nota2, nota3, nota4){
+    console.log(somarNotas(8, '6', 9, 3))
 }
-
-textReturn('Olá, estou sendo retornado pela função')
